67ffe851fd53e12029394d19d998b6f783ffe076
Hentiofyle <92610008+Hentiofyle@users.noreply.github.com>
2022-04-07 17:33:53 +0200

    Update photographer.js
    class phtographer to create object 
    get picture to get portrait 
    get localisation 
    ++ map over data.photographers and set html dynamicly

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -15,3 +15,47 @@ function photographerFactory(data) {
     }
     return { name, picture, getUserCardDOM }
 }
+
+class Photographer {
+   
+    constructor(data) {
+        this._id = data.id
+        this._city = data.city
+        this._country = data.country
+        this._name = data.name
+        this._picture = data.portrait
+        this._price = data.price
+        this._tagline = data.tagline
+        this._tags = data.tags
+
+    }
+   
+    get picture() {
+        return `../assets/photographers/${this._picture}`
+    }
+
+   
+    get localization() {
+        return `${this._city}, ${this._country}`
+    }
+}
+
+let photographers = data.photographers;
+        photographers.map(photographe => {
+            let sectionPhotographers = document.getElementById('photographers');
+            let articlePhotographers = document.createElement('article');
+            let templatePhotographer = `
+            <a href="photographers.html?id=${photographe.id}" title="${photographe.name}">
+                <img src="${photographe.portrait}" alt="${photographe.alt}">
+                <h2 class="name">${photographe.name}</h2>
+            </a>
+            <p class="location">${photographe.city}, ${photographe.country}</p>
+            <p class="tagline">${photographe.tagline}</p>
+            <p class="price">${photographe.price}€/jour</p>
+            <ul class="filter">${photographe.tags.map(tag =>
+                `<li data-filter="${tag}">#${tag}</li>`).join(" ")}</ul> 
+            `
+
+            sectionPhotographers.appendChild(articlePhotographers);
+            articlePhotographers.innerHTML = templatePhotographer;
+        })
