306f0e72254dbea6f183b55a84eb241ad5edc2c7
Hentiofyle <92610008+Hentiofyle@users.noreply.github.com>
2022-04-21 13:37:23 +0200

    Update photographer.js
    change in the getUserDom card function

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,19 +1,31 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, tagline, city, country, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `medias/${portrait}`;
 
-    function getUserCardDOM() {
+       function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        //const h2 = document.createElement( 'h2' );
+        //h2.textContent = name;
+       
+        //article.appendChild(img);
+        //article.appendChild(h2);
+        article.innerHTML = `
+       <img class="photographer_thumbnail__picture"
+       src="medias/${portrait}"
+       alt="${name}'s thumbnail picture" />
+      <h2 id ="personalPage"class="photographer_name">${name}</h2>
+      <h3 class="photographer_location">${city}, ${country}</h3>
+      <p class="photographer_desc">${tagline}</p>
+      <p class="photographer_price">${price}€/jour</p>`
+     
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, tagline, city, country, price,  getUserCardDOM }
+     
+    
 }
 
 class Photographer {
